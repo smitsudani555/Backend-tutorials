1bbbb81f9f70acfa3767f572841e1409288f2e78
smitsudani555 <smitsudani555@gmail.com>
2025-03-12 12:24:58 +0530

    auth middleware and route created

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,23 @@ import { User } from '../models/user.models.js'
 import { uploadOnCloudinary } from '../utils/cloudinary.js'
 import { ApiResponce } from '../utils/ApiResponce.js'
 
+
+const generateAccessTokenAndgenerateRefreshToken = async(userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave: false})
+
+        return {accessToken,refreshToken}
+
+    } catch (error) {
+        throw new ApiError(500,"samething went wrong while generation refresh and access token")
+    }
+}
+
 const registerUser = asyncHandler(async(req,res) =>{
     // get user details from frontend
     // validation - not empty
@@ -72,4 +89,82 @@ const registerUser = asyncHandler(async(req,res) =>{
 
 })
 
-export {registerUser}
+
+const loginUser = asyncHandler(async(req,res) => {
+    // req body -> data
+    // username or email
+    //find the user
+    //password check
+    //access and referesh token
+    //send cookie
+
+    const {email,username,password} = req.body
+
+    if(!username || !email){
+        throw new ApiError(400,"username and email is required")
+    }
+
+    const user = await User.findOne({
+        $or: [{username},{email}]
+    })
+
+    if(!user){
+        throw new ApiError(404,"user does not exist")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+    if(!isPasswordValid){
+        throw new ApiError(401,"password incorrect")
+    }
+
+    const {accessToken, refreshToken} = await generateAccessTokenAndgenerateRefreshToken(user._id)
+
+    const loggedInUser =await User.findById(user._id).select("-password -refreshToken")
+    
+    const options = {
+        httpOnly : true,
+        secure: true
+    }
+
+    return res.status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(
+        new ApiResponce(
+            200,
+            {
+                user: loggedInUser,accessToken,refreshToken
+            },
+            "User LoggedIn successfully"
+        )
+    )
+})
+
+
+const logoutUser = asyncHandler(async(req,res) => {
+    User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly : true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken",options)
+    .json(new ApiResponce(200,{},"User logged out"))
+})
+
+export {registerUser,loginUser,logoutUser}
+
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import {upload} from '../middelwares/multer.middleware.js'
+import { verifyJWT } from "../middelwares/auth.middleware.js";
 
 const router = Router()
 
@@ -19,5 +20,9 @@ router.route('/register').post(
     registerUser
 )
 
+router.route('/login').post(loginUser)
+
+//secured routes
+router.route('/logout').post(verifyJWT,logoutUser)
 
 export default router
